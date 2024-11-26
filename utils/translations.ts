127d1b95b2d39e1aa1295683cf09e6c70c6f8ce6
Cris Lomas <cristian.lomas.a@gmail.com>
2024-11-25 21:40:52 -0300

    Agregado de clase Translations:  mensajes traducidos a ambos idiomas

---
+++ utils/translations.ts
@@ -0,0 +1,49 @@
+export type Translations = {
+    en : LanguageStrings,
+    es : LanguageStrings
+}
+
+export type LanguageStrings = {
+    tasks: string,
+    addTask: string,
+    menu: string,
+    categories: string,
+    addCategory: string,
+    newTaskPlaceholder: string,
+    complete: string,
+    uncomplete: string,
+    delete: string,
+    noTasks: string,
+    settings : string
+}
+
+const translations : Translations = {
+    en: {
+      tasks: "Tasks",
+      addTask: "Add Task",
+      menu: "Menu",
+      categories: "Categories",
+      addCategory: "Add category",
+      newTaskPlaceholder: "New task",
+      complete: "Complete",
+      uncomplete: "Uncomplete",
+      delete: "Delete",
+      noTasks: "No tasks available",
+      settings: "Settings"
+    },
+    es: {
+      tasks: "Tareas",
+      addTask: "Agregar Tarea",
+      menu: "Menú",
+      categories: "Categorías",
+      addCategory: "Agregar categoría",
+      newTaskPlaceholder: "Nueva tarea",
+      complete: "Completar",
+      uncomplete: "Desmarcar",
+      delete: "Eliminar",
+      noTasks: "No hay tareas disponibles",
+      settings: "Opciones"
+    },
+  }
+  
+  export default translations
