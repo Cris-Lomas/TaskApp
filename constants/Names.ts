cfac3079ced466aa89ccdfcbd428fe0f47c22f81
Cris Lomas <cristian.lomas.a@gmail.com>
2024-11-25 21:39:26 -0300

    Agregado de constantes - Names y Sizes

---
+++ constants/Names.ts
@@ -0,0 +1,3 @@
+export const START_TASK_KEY = "tarea-"
+export const START_TASK_CATEGORY_KEY = "categoria-"
+export const LAST_ID_KEY = "last-task-id"
---
+++ constants/Sizes.ts
@@ -0,0 +1,3 @@
+export const HEADER_HEIGHT = 25
+
+export const MIN_PADDING = 4
