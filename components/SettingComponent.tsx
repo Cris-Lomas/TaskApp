9950402a88ac2f524f62605781c87582b37d3674
Cris Lomas <cristian.lomas.a@gmail.com>
2024-11-29 16:56:52 -0300

    Creacion de SettingComponent

---
+++ components/SettingComponent.tsx
@@ -0,0 +1,41 @@
+import { StyleSheet, TouchableOpacity } from 'react-native';
+import { ThemedView } from '@/components/ThemedView';
+import { HEADER_HEIGHT, MIN_PADDING } from '@/constants/Sizes';
+import { ThemedText } from './ThemedText';
+import { Task } from '@/domain/Task';
+
+type Props = {
+  name : string
+  action : () => void
+};
+
+export default function SettingComponent({ name, action }: Props) {
+
+  return (
+    <>
+    <TouchableOpacity onPress={action} style={styles.container}>
+      <ThemedText type="title">
+        {name}
+      </ThemedText>
+    </TouchableOpacity>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    height: HEADER_HEIGHT * 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: MIN_PADDING
+  },
+  content: {
+    flex: 1,
+    padding: 32,
+    gap: 16,
+    overflow: 'hidden',
+  },
+});
