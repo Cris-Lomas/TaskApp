ef3a0a387a49eb7ab7269bd932c3d7ab96cc9575
Cris Lomas <cristian.lomas.a@gmail.com>
2024-12-01 21:17:28 -0300

    Agregado de componente toast

---
+++ utils/toast.ts
@@ -0,0 +1,15 @@
+import Toast from "react-native-toast-message"
+
+export const showSuccess = (msg : string) => {
+    Toast.show({
+        type: "success",
+        text1: msg
+    })
+}
+
+export const showError = (msg : string) => {
+    Toast.show({
+        type: "error",
+        text1: msg
+    })
+}
